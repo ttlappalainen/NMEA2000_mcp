53cf6eae69cb71f9ac87f037ab1dfd3722e98776
ttlappalainen <timo.lappalainen@kave.fi>
2020-03-29 08:05:30 +0300

    Just rename of internal variable.

--- NMEA2000_mcp.cpp
+++ NMEA2000_mcp.cpp
@@ -242,10 +242,10 @@ void tNMEA2000_mcp::InterruptHandler() {
         //N2kCAN.readMsgBuf(&len,buf);
         //id=N2kCAN.getCanId();
         //pRxBuffer->AddFrame(id,len,buf);
-      } else { // Buffer full, skip char
-        tCANFrame MissFrame;
+      } else { // Buffer full, skip frame
+        tCANFrame FrameToSkip;
         byte ext,rtr;
-        N2kCAN.readMsgBufID(status,&(MissFrame.id),&ext,&rtr,&(MissFrame.len),MissFrame.buf);
+        N2kCAN.readMsgBufID(status,&(FrameToSkip.id),&ext,&rtr,&(FrameToSkip.len),FrameToSkip.buf);
       }
     }
 
